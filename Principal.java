9275c6c81dca95cba5b4ad61c2eac7ab49d727d9
mr27cris <1727442@alu.murciaeduca.es>
2025-02-05 10:33:59 +0100

    Clase Usuario y método para añadir usuarios añadido

--- Principal.java
+++ Principal.java
@@ -6,15 +6,47 @@
 * Fecha: 05/02/2025
 * 
 */
+import java.util.Scanner;
 import java.time.LocalDateTime;
-public class Principal{
-	public static void main(String[] args){
-		System.out.println("Hola Mundo!!");
-		mostrarFechahora();
-	}
+public class Principal {
+  public static void main(String[] args) {
 
+    System.out.println("Hola Mundo!!");
+    mostrarFechahora();
 
-	public static void mostrarFechahora() {
-        System.out.println("Fecha y hora actual: " + LocalDateTime.now());
+    Usuario[] usuarios = crearUsuarios();
+    System.out.println("\n Usuarios introducidos: ");
+    for (int i = 0; i < usuarios.length; i++) {
+      System.out.println(usuarios[i].nombre + " " + usuarios[i].apellidos + " - " + usuarios[i].email);
     }
+
+  }
+
+  public static void mostrarFechahora() {
+    System.out.println("Fecha y hora actual: " + LocalDateTime.now());
+  }
+
+  public static Usuario[] crearUsuarios() {
+    Scanner kb = new Scanner(System.in);
+
+    System.out.print("Introduzca el número de usuarios: ");
+    int num = kb.nextInt();
+    kb.nextLine();
+
+    Usuarios[] usuarios = new Usuario[num];
+
+    for (int i = 0; i < num; i++) {
+      System.out.println("Datos del usuario " + (i + 1) + ":");
+      System.out.print("Nombre: ");
+      usuarios[i].nombre = kb.nextLine();
+      System.out.print("Apellidos: ");
+      usuarios[i].apellidos = kb.nextLine();
+      System.out.print("Email: ");
+      usuarios[i].email = kb.nextLine();
+    }
+    kb.close();
+    return usuarios;
+
+  }
+
 }
--- README.md
+++ README.md
@@ -11,4 +11,8 @@ Cristina Moñino Rubio
 Se trata de la primera práctica sobre github para la asignatura Entornos de Desarrollo. Esta práctica tiene como **objetivo** conocer más sobre el tan usado por todos, *github*.
 
 ## Principal:
-Código para mostrar HolaMundo y además imprimir la fecha y hora actual con un método llamado desde el main. 
+Código para mostrar HolaMundo, imprimir la fecha y hora actual y llamada a la clase Usuario que contiene las variables 
+
+## Usuario:
+Clase usuario en la que estan definidas las variables 
+nombre, apellidos y email. Posteriormente se usan para la creación de un usuario en el Principal. 
