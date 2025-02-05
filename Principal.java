49efd4003adc86dad631f5fcb698e97d39e91948
mr27cris <1727442@alu.murciaeduca.es>
2025-02-05 10:07:07 +0100

    Método para mostrar fecha y hora añadido

--- Principal.java
+++ Principal.java
@@ -6,9 +6,15 @@
 * Fecha: 05/02/2025
 * 
 */
-
+import java.time.LocalDateTime;
 public class Principal{
 	public static void main(String[] args){
-		System.out.print("Hola Mundo!!");
+		System.out.println("Hola Mundo!!");
+		mostrarFechahora();
 	}
+
+
+	public static void mostrarFechahora() {
+        System.out.println("Fecha y hora actual: " + LocalDateTime.now());
+    }
 }
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Cristina Moñino Rubio
 ### Tarea 4.1 
 
 Se trata de la primera práctica sobre github para la asignatura Entornos de Desarrollo. Esta práctica tiene como **objetivo** conocer más sobre el tan usado por todos, *github*.
+
+## Principal:
+Código para mostrar HolaMundo y además imprimir la fecha y hora actual con un método llamado desde el main. 
